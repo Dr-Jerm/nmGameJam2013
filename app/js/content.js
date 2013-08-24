a01c77e23097362db76d5c49379503b08d144b41
egeusz <odowd450@158-106-MAC20.local>
2013-08-24 12:14:50 -0600

    commit summery

--- app/js/content.js
+++ app/js/content.js
@@ -1,4 +1,39 @@
 var images = new Array();
 
+var imageSrcList = new Array();
+var imageCounter = 0;  
+
+function LoadImage(_imageSrc)
+{
+	var img = new Image();
+
+	img.onload = LoadImageCallBack(); 
+	img.src = "images/"+_imageSrc; 
+	images[_imageSrc] = img;
+}
+
+function LoadImageCallBack()
+{
+	imageCounter++; 
+	console.log("img counter "+imageCounter); 
+	if (imageCounter >= imageSrcList.length) 
+	{
+		console.log("All images loaded.");
+
+	}
+
+}
+
+imageSrcList.push("Grumpy-Cat.jpg");
+imageSrcList.push("sperm.png");
+imageSrcList.push("Egg.png");
+imageSrcList.push("BGsecondary001.png");
+imageSrcList.push("BGfull001.png");
+
+
+for (imgsrc in imageSrcList)
+{
+	LoadImage(imageSrcList[imgsrc]);
+}
 
 
