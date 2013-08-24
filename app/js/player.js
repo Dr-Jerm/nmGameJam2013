1d38a9609bb4a859f0047597e8c0d3d4a428df1d
otionstem <odowd450@158-106-MAC19.local>
2013-08-24 00:33:31 -0600

    added variables

--- app/js/player.js
+++ app/js/player.js
@@ -1,13 +1,25 @@
 function Player(_sperm)
 {
 
-	this.sperm = _sperm; 
+	this.sperm = _sperm;
+
+	var posX = 0;
+	var posY = 0;
+	var rot = 0;
+
+	var velX = 0;
+	var velY = 0;
+	var rotVel = 0;
 
 	this.update = function()
 	{
 
 		console.log("playerupdate");
 		this.sperm.update();
+
+		posX += velX;
+		posY += velY;
+		rot += rotVel;
 
 	}
 }
