43b5babd13f222abbe83b30cadc83710304f0f6c
egeusz <odowd450@158-106-MAC20.local>
2013-08-24 00:32:26 -0600

    fixed conflicts

--- app/js/player.js
+++ app/js/player.js
@@ -5,19 +5,9 @@ function Player(_sperm)
 
 	this.update = function()
 	{
-<<<<<<< HEAD
-		//console.log("playerupdate");
 
-
-	}
-
-
-
-=======
 		console.log("playerupdate");
 		this.sperm.update();
 
 	}
-
->>>>>>> 4606f3258ffe9d028cb6f61389fe646051abf4fa
 }
