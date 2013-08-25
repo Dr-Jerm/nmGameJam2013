78071670664f44c4fca7e5a0537eaece0f90bb03
egeusz <odowd450@158-106-MAC20.local>
2013-08-25 17:24:08 -0600

    fixed image load order

--- app/js/content.js
+++ app/js/content.js
@@ -27,20 +27,20 @@ function LoadImageCallBack()
 
 }
 
-imageSrcList.push("Grumpy-Cat.jpg");
+imageSrcList.push("particulate001.png");
+imageSrcList.push("particulate002.png");
+imageSrcList.push("particulate003.png");
+//imageSrcList.push("Grumpy-Cat.jpg");
 imageSrcList.push("sperm.png");
-imageSrcList.push("Egg.png");
+//imageSrcList.push("Egg.png");
 imageSrcList.push("newEggMain.png");
 imageSrcList.push("newEggOuterRing1.png");
 imageSrcList.push("newEggInsidePartBig.png");
 imageSrcList.push("newEggInsidePartSml.png");
 imageSrcList.push("newEggInsidePartSml2.png");
-imageSrcList.push("BGsecondary001.png");
+//imageSrcList.push("BGsecondary001.png");
 imageSrcList.push("BGfull001.png");
 
-imageSrcList.push("particulate001.png");
-imageSrcList.push("particulate002.png");
-imageSrcList.push("particulate003.png");
 
 // imageSrcList.push("particulars002.png");
 // imageSrcList.push("particulars003.png");
