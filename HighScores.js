ce7faea80463fd0b4664540017bf4b1f296bf285
Vita Pluvia <vitapluvia@gmail.com>
2013-08-25 16:10:42 -0700

    added rank to index.ejs

--- HighScores.js
+++ HighScores.js
@@ -1,69 +1,68 @@
 // HighScore Leaderboard for Game:
+
 var HighScores = function () {
 
   // Some Mock Data:
   this.PAGE_LENGTH = 15; // Amount of scores displayed on a page.
-  this.ranks = [1, 2, 3, 4, 5]
+  ranks = []; for (x=0;x<15;x++) ranks.push(x+1) 
   this.all_scores = {} // Stores Score information for Users
   // if username > 14_chars : username = username[0:11] + '...'
-
-  // For simplicity, only show highest scores...
-  // Highest Scores:
-
-  // @TODO:
-  //  - Need current player from player obj.
-  //  - Insert Latest Scores & Compare
-  //  - Find Top Scores & Sort to rank.
-  //  - ...
 
   this.insertScores = function(players) {
     /* This takes a list of players and
        adds them to all_scores to display. */
+    // Triggers on leaderboard win condition.
     for (i in players) {
-      this.all_scores[players[i].name] = players[i].score;
+      var name = players[i].name;
+      var score = players[i].score;
+      if (score == undefined) score = 0; 
+      this.all_scores[name] = score;
     }
+    this.findHighestScores();
+    console.log(this.all_scores);
+
+    // Somehow need to update the ejs templates:
+    // res.update() maybe on server.js for all_scores variable.
   }
-  this.insertScores([
-    {name:'testy', score:45345},
-    {name:'sdf', score:1},
-    {name:'billy', score:82448},
-    {name:'spermicide', score:7287},
-    {name:'swimmer', score:92313218},
-    {name:'runner', score:4},
-    {name:'leonard', score:831},
-    {name:'steve', score:23092},
-  ]);
 
   this.findHighestScores = function() {
+
+    // Store all scores:
     var scores = [];
     for (score in this.all_scores) { 
       scores.push(this.all_scores[score]);
     }
-
-    // Sorting and taking Top Scores:
+    // Sort and cut down to top scores:
     scores.sort(function(a,b){return a<b})
     scores = scores.slice(0, this.PAGE_LENGTH);
-    console.log(scores);
 
-    // Need to re-reference the player objects...
+
+
+
+
+    // Need to reference the player objects from the scores list...
+    //for (score in this.all_scores) { 
+    //  for (s in scores) {
+    //    cur_score = this.all_scores[score]
+    //    if (cur_score == s) {
+    //      // ljk
+    //    }
+    //  }
+    //}
     new_highscores = [];
-    console.log("New Highscores", new_highscores);
+    //console.log(scores);
+    //console.log("New Highscores", new_highscores);
   };
-  this.findHighestScores();
+
+  //  this.winning = function() {
+  //    // How to access DOM from this scope ?
+  //    // On Win: Can just should trigger $('#leaderboard').show();
+  //    // Add Play Again ?
+  //  }
 
 
-//  this.getScorePage = function() {
-//    /* Grab the current page and return the results. */
-//    // This could be done nicely with a redis db.
-//    // Quick ex.: http://dickeyxxx.com/the-great-redis-misapprehension
-//    // page = range(s-e) # With PAGE_LENGTH steps.
-//    // return page
-//  }
 
-//  this.drawLeaderboard = function() {
-//    // On Win: Can just should trigger $('#leaderboard').show();
-//    // Add Play Again ?
-//  }
+  // this.findHighestScores();
 
 }
 module.exports = new HighScores();
